5a7b2a253d0b3c65b420dd2f0ab47e6900625982
BhagyashreeSail <bhagyashreesail08@gmail.com>
2020-08-08 19:09:05 +0530

    implemented partially

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatInputModule }  from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { FormsModule }   from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
